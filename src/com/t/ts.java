71709f854b44ed8105d9c297649077bf737e5502
LuLuKa <1272196672@qq.com>
2022-04-15 22:35:30 +0800

    491.递增子序列

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -9,40 +9,29 @@ public class ts {
 }
 
 class Solution {
-    List<String> result = new ArrayList<>();
-    StringBuilder ip = new StringBuilder();
-    int num = 0;
-    public List<String> restoreIpAddresses(String s) {
-        if (s.length() > 12){
-            return result;
-        }
-        backTracing(s, 0);
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backTracing(nums, 0);
         return result;
     }
 
-    public void backTracing(String s, int startIndex){
-        if (num == 4 && startIndex == s.length()){
-            result.add(ip + "");
-            return;
+    public void backTracing(int[] nums, int startIndex){
+        if (path.size() >= 2){
+            result.add(new ArrayList<>(path));
         }
-        if (num == 4){
-            return;
-        }
-        for (int i = startIndex; i < s.length() && i - startIndex < 3 && Integer.parseInt(s.substring(startIndex, i + 1)) >= 0 && Integer.parseInt(s.substring(startIndex, i + 1)) <= 255 && s.length() - startIndex - 1 <= 4 * (4 - num); i++) {
-            if (i - startIndex > 0 && s.charAt(startIndex) == '0'){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = startIndex; i < nums.length; i++) {
+            if (path.size() != 0 && nums[i] < path.getLast()){
                 continue;
             }
-            ip.append(s.substring(startIndex, i + 1));
-            if (num < 3){
-                ip.append('.');
+            if (map.getOrDefault(nums[i], 0) != 0){
+                continue;
             }
-            num++;
-            backTracing(s, i + 1);
-            num--;
-            if (num < 3){
-                ip.deleteCharAt(ip.length() - 1);
-            }
-            ip.delete(startIndex + num, i + num + 1);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            path.add(nums[i]);
+            backTracing(nums, i + 1);
+            path.removeLast();
         }
     }
 }
