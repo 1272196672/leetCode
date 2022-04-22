4db7dcd4a8f82d02874c3fad79f2256b0213b5cb
LuLuKa <1272196672@qq.com>
2022-04-22 20:34:05 +0800

    经典01背包

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -4,19 +4,21 @@ import java.util.*;
 
 public class ts {
     public static void main(String[] args) {
-
+        new Solution().canPartition();
     }
 }
 
 class Solution {
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i ; j++) {
-                dp[i] += dp[i - j] * dp[j - 1];
+    public void canPartition() {
+        int[] weight = new int[]{1,3,4};
+        int[] value = new int[]{15,20,30};
+        int bagWeight = 4;
+        int[] dp = new int[bagWeight+ 1];
+        for (int i = 0; i < weight.length; i++) {
+            for (int j = bagWeight; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
+            System.out.println(Arrays.toString(dp));
         }
-        return dp[n];
     }
 }
