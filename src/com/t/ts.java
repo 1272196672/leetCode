199f72efdbb7a11ef7ae1f6c6259f3352f107555
LuLuKa <1272196672@qq.com>
2022-04-22 15:12:49 +0800

    96. 不同的二叉搜索树,建议深度理解

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -9,12 +9,12 @@ public class ts {
 }
 
 class Solution {
-    public int integerBreak(int n) {
+    public int numTrees(int n) {
         int[] dp = new int[n + 1];
-        dp[2] = 1;
-        for (int i = 3; i <= n; i++) {
-            for (int j = 1; j < i - 1; j++) {
-                dp[i] = Math.max(dp[i], Math.max(dp[i - j] * j, (i - j) * j));
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                dp[i] += dp[i - j] * dp[j - 1];
             }
         }
         return dp[n];
