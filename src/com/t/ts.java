9cc74c4c4838f9c7c6a225d8b7f4bba84a33bc62
LuLuKa <1272196672@qq.com>
2022-04-23 21:03:37 +0800

    494. 目标和，01背包

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -9,55 +9,24 @@ public class ts {
 }
 
 class Solution {
-    int[] used;
-    public boolean makesquare(int[] matchsticks) {
+    public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
-        for (int i : matchsticks) {
-            sum += i;
+        for (int num : nums) {
+            sum += num;
         }
-        if (sum % 4 != 0){
-            return false;
+        if ((sum + target) % 2 != 0 || Math.abs(target) > sum){
+            return 0;
         }
-        int target = sum / 4;
-        Arrays.sort(matchsticks);
-        used = new int[matchsticks.length];
-        //反序遍历火柴，大的可以先剪
-        return backtracking(matchsticks, new int[4], target, matchsticks.length - 1);
-    }
+        int backWeight = (sum + target) / 2;
+        int[] dp = new int[backWeight + 1];
+        dp[0] = 1;
 
-    public boolean backtracking(int[] matchsticks, int[] sides, int target, int startIndex){
-        //所有火柴使用完，判断四边是否相等，相等则true
-        if (startIndex == -1){
-            for (int side : sides) {
-                if (side != target){
-                    return false;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            //走下面的for则是能放进去，本身方法数量加上重量减去此num的数量
+            for (int j = backWeight; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
             }
-            return true;
         }
-
-        //外层遍历四边，内层遍历火柴，相当于将每根火柴按顺序和4边比较，有空位则放入
-        for (int i = 0; i < sides.length; i++) {
-            //火柴长度长于此空位，尝试下一空位
-            if (sides[i] + matchsticks[startIndex] > target){
-                continue;
-            }
-            //相同火柴长度的，前面用过，没放进空位，这跟长度相同的也不用比较了
-            if (startIndex < matchsticks.length - 1 && matchsticks[startIndex] == matchsticks[startIndex + 1] && used[startIndex + 1] == 0){
-                continue;
-            }
-            //这根火柴过长，所有空位都腾出来也不够放，直接break
-            if (i > 0 && startIndex == matchsticks.length - 1){
-                break;
-            }
-            sides[i] += matchsticks[startIndex];
-            used[startIndex] = 1;
-            if (backtracking(matchsticks, sides, target, startIndex - 1)){
-                return true;
-            }
-            used[startIndex] = 0;
-            sides[i] -= matchsticks[startIndex];
-        }
-        return false;
+        return dp[backWeight];
     }
-}   
+}
