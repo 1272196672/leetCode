09befa63b52ba5b8cf4af9941893baff656353b8
LuLuKa <1272196672@qq.com>
2022-08-09 16:24:33 +0800

    189. 轮转数组(3次反转数组)

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -1,58 +1,28 @@
 package com.t;
 
+import javax.lang.model.type.UnionType;
 import java.util.*;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class ts {
     public static void main(String[] args) {
-
+        new Solution().rotate(new int[]{1,2,3,4,5,6}, 3);
     }
 }
 
-class Solution {
-    boolean[] used;
-    public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum = 0;
-        for (int i : nums) {
-            sum += i;
-        }
-        if (sum % k != 0){
-            return false;
-        }
-        int target = sum / k;
-        Arrays.sort(nums);
-        used = new boolean[nums.length];
-        return backtracking(nums, 0, k, target, nums.length - 1);
-    }
 
-    public boolean backtracking(int[] nums, int sum, int k, int target, int startIndex){
-//        k个背包都装满了
-        if (k == 0){
-            return true;
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    void reverse(int[] nums, int start, int end){
+        for (int i = start, j = end; i < j; i++, j--) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
-//        当前的背包满了，切换下一个背包，从头开始遍历物品，背包少一个
-        if (sum == target){
-            return backtracking(nums, 0, k - 1, target, nums.length - 1);
-        }
-//        从大到小遍历物品
-        for (int i = startIndex; i >= 0; i--) {
-//            树枝去重
-            if (used[i]){
-                continue;
-            }
-//            sum大于target，跳过
-            if (sum + nums[i] > target){
-                continue;
-            }
-//            数层去重
-            if (i < nums.length - 2 && nums[i] == nums[i + 1] && !used[i + 1]){
-                continue;
-            }
-            used[i] = true;
-            if (backtracking(nums, sum + nums[i], k, target, i - 1)){
-                return true;
-            }
-            used[i] = false;
-        }
-        return false;
     }
 }
--- src/com/ts.java
+++ src/com/ts.java
@@ -1,10 +1,48 @@
 package com;
 
+import java.text.DecimalFormat;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ts {
-    public static void main(String[] args) {
-        System.out.println(8 % 3);
+    public static void main(String s[]) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String x = scanner.next();
+            if (x.length() % 2 == 0){
+                String[] strings = new String[x.length() / 2];
+                int index = 0;
+                for (int i = 0; i < strings.length; i = i + 2) {
+                    strings[index] = x.substring(i, i + 2);
+                }
+                int sum = 0;
+                for (int i = 0; i < strings.length; i++) {
+                    sum += Integer.parseInt(strings[i]);
+                }
+            }
+        }
+    }
+
+    class Main {
+        public static void main(String args[]) {
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                int n = scanner.nextInt();
+                int m = scanner.nextInt();
+                int[] nums = new int[n];
+                int sum = 0;
+                for (int i = 0; i < n; i++) {
+                    nums[i] = scanner.nextInt();
+                    sum += nums[i];
+                }
+                int bagWeight = sum / m;
+                int[] dp = new int[bagWeight];
+                for (int i = 0; i < n; i++) {
+                    for (int j = bagWeight; j >= nums[i]; j--) {
+                        dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+                    }
+                }
+                System.out.println(2 * bagWeight - dp[bagWeight - 1]);
+            }
+        }
     }
 }
