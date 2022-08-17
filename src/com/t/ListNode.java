23384abba106ed80e263d6d52915e3c4ed067280
LuLuKa <1272196672@qq.com>
2022-08-17 13:45:05 +0800

    673. 最长递增子序列的个数 # 建议深入理解

---
+++ src/com/t/ListNode.java
@@ -0,0 +1,22 @@
+package com.t;
+
+/**
+ * @author 林子翔
+ * @since 2022 08 2022/8/10
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -1,28 +1,40 @@
 package com.t;
 
-import javax.lang.model.type.UnionType;
 import java.util.*;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class ts {
     public static void main(String[] args) {
-        new Solution().rotate(new int[]{1,2,3,4,5,6}, 3);
+
     }
 }
 
-
 class Solution {
-    public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
-    void reverse(int[] nums, int start, int end){
-        for (int i = start, j = end; i < j; i++, j--) {
-            int tmp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = tmp;
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        int res = 0;
+        int maxLen = 1;
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    if (dp[j] + 1 > dp[i]){
+                        count[i] = count[j];
+                    }
+                    else if (dp[j] + 1 == dp[i]){
+                        count[i] += count[j];
+                    }
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
         }
+        for (int i = 0; i < nums.length; i++) {
+            if (dp[i] == maxLen){
+                res += count[i];
+            }
+        }
+        return res;
     }
 }
