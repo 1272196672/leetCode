de6cff48112adb1c1e81d9adc685f23a840f5fac
LuLuKa <1272196672@qq.com>
2022-08-23 15:26:48 +0800

    841. 钥匙和房间 图论，有向图，深度优先

--- src/com/t/ts.java
+++ src/com/t/ts.java
@@ -9,32 +9,27 @@ public class ts {
 }
 
 class Solution {
-    public int findNumberOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int[] count = new int[nums.length];
-        int res = 0;
-        int maxLen = 1;
-        Arrays.fill(dp, 1);
-        Arrays.fill(count, 1);
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]){
-                    if (dp[j] + 1 > dp[i]){
-                        count[i] = count[j];
-                    }
-                    else if (dp[j] + 1 == dp[i]){
-                        count[i] += count[j];
-                    }
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        ArrayList<Boolean> visited = new ArrayList<>(){{
+            for (int i = 0; i < rooms.size(); i++) {
+                add(false);
             }
-            maxLen = Math.max(maxLen, dp[i]);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (dp[i] == maxLen){
-                res += count[i];
+        }};
+        backtracking(0, rooms, visited);
+        for (Boolean aBoolean : visited) {
+            if (!aBoolean){
+                return false;
             }
         }
-        return res;
+        return true;
+    }
+    void backtracking(int key, List<List<Integer>> rooms, ArrayList<Boolean> visited){
+        if (visited.get(key)){
+            return;
+        }
+        visited.set(key, true);
+        for (Integer integer : rooms.get(key)) {
+            backtracking(integer, rooms, visited);
+        }
     }
 }
